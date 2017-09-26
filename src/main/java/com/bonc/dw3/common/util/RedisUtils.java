e8071efa57a3761c7c928b5cbff6e6e779daf658
zhaogang <zhaogang@bonc.com.cn>
2017-09-26 11:45:17 +0800

    改造工具类为多线程查询redis

--- src/main/java/com/bonc/dw3/common/util/RedisUtils.java
+++ src/main/java/com/bonc/dw3/common/util/RedisUtils.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -27,15 +28,90 @@ public class RedisUtils {
     }
 
     public static List<Object> getResult(List<String> keyList){
-        JedisCluster jc = new JedisCluster(nodes);
-        JedisClusterPipeline jcp = JedisClusterPipeline.pipelined(jc);
-        jcp.refreshCluster();
-        List<Object> batchResult = null;
-        for (String key:keyList){
-            jcp.get(key);
+        JedisCluster jedisCluster = null;
+        List<Object> batchResult = new ArrayList<>();
+        try {
+            jedisCluster = new JedisCluster(nodes);
+            /*JedisClusterPipeline jcp = JedisClusterPipeline.pipelined(jc);
+            jcp.refreshCluster();
+            List<Object> batchResult = null;
+            for (String key:keyList){
+                jcp.get(key);
+            }
+            batchResult = jcp.syncAndReturnAll();*/
+            int threadCount = 50;
+            RedisThread[] threadArr = new RedisThread[threadCount];
+
+            int count = keyList.size();
+            int avg = count / threadCount;
+            int start = 0;
+            for (int i = 0; i < threadCount; i++) {
+                List<String> listTmp = new ArrayList<>();
+                if (i == threadCount - 1) {
+                    listTmp = keyList.subList(start, count);
+                    threadArr[i] = new RedisThread(listTmp, jedisCluster);
+                    threadArr[i].start();
+                    //System.out.println(start+"###"+count);
+                    break;
+                }
+                listTmp = keyList.subList(start, start + avg);
+                threadArr[i] = new RedisThread(listTmp, jedisCluster);
+                threadArr[i].start();
+                //System.out.println(start+"###"+(start+avg));
+                start = start + avg;
+            }
+
+            for (int i = 0; i < threadCount; i++) {
+                threadArr[i].join();
+            }
+            for (int i = 0; i < threadCount; i++) {
+                if(null != threadArr[i]){
+                    batchResult.addAll(threadArr[i].result);
+                }
+            }
+        }catch (Exception e){
+            log.error("查询redis出错", e);
+        }finally {
+            if(jedisCluster != null){
+                try {
+                    jedisCluster.close();
+                } catch (IOException e) {
+                    log.error("", e);
+                }
+
+            }
         }
-        batchResult = jcp.syncAndReturnAll();
         return batchResult;
     }
 
+    static class RedisThread extends Thread {
+
+        private Logger log = LoggerFactory.getLogger(RedisThread.class);
+
+
+        List<String> list;
+        JedisClusterPipeline jcp;
+        JedisCluster jedisCluster;
+        List<String> rs = new ArrayList<>();
+        List<Object> result = null;
+
+        public RedisThread(List<String> list, JedisCluster jedisCluster){
+            this.list = list;
+            this.jedisCluster = jedisCluster;
+        }
+
+        @Override
+        public void run() {
+            //List<Object> result = new ArrayList<>();
+            JedisClusterPipeline jcp = JedisClusterPipeline.pipelined(jedisCluster);
+            long start = System.currentTimeMillis();
+            System.out.println(list.size());
+            for(int i = 0;i<list.size();i++){
+                jcp.get(list.get(i));
+            }
+            result = jcp.syncAndReturnAll();
+            jcp.close();
+            //System.out.println("线程耗时"+(System.currentTimeMillis()-start)+"ms,有"+result.size()+"条返回结果");
+        }
+    }
 }
