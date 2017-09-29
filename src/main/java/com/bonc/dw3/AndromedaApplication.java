8ae09b5043b06b889d1197885d38b933dbf48b97
zhaogang <zhaogang@bonc.com.cn>
2017-09-29 15:55:43 +0800

    fastjson处理json串提高效率

--- src/main/java/com/bonc/dw3/AndromedaApplication.java
+++ src/main/java/com/bonc/dw3/AndromedaApplication.java
@@ -1,9 +1,13 @@
 package com.bonc.dw3;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.bonc.dw3.common.datasource.DynamicDataSourceRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -11,8 +15,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @Import(DynamicDataSourceRegister.class)
@@ -36,5 +45,23 @@ public class AndromedaApplication {
 	@LoadBalanced
 	RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters(){
+		//1.需要定义一个convert转换消息的对象;
+		FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+		//2:添加fastJson的配置信息;
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		//3处理中文乱码问题
+		List<MediaType> fastMediaTypes = new ArrayList<>();
+		fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+		//4.在convert中添加配置信息.
+		fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
+		fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+		HttpMessageConverter<?> converter = fastJsonHttpMessageConverter;
+		return new HttpMessageConverters(converter);
+
 	}
 }
